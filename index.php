4379acb97889bbc3ead71b00b53114455a5014f3
Jeremy Mathieu <g3raphacs@gmail.com>
2020-05-29 21:37:54 +0200

    responsive navbar

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
     <header>
         <div id="navbar">
-            <nav class="container flexspaceb m0child wrap">
+            <nav class="container flexspaceb m0child">
                 <div id="logoburger" class="flexspaceb">
                         <div id="logo" class="flex">
                             <div><img src="/media/multiply-logo.png" alt="" width="100" height="100"></div>
@@ -20,7 +20,7 @@
                         <div id="burger" class="flex"><img src="/media/burger.svg" alt="" width="40" height="40"></div>
                 </div>
                 
-                <menu class="flexspaceb f-med">
+                <menu class="flexspaceev f-med">
                     <li><a href="index.php">Accueil</a></li>
                     <li><a href="tables.php">Tables</a></li>
                     <li><a href="tables2.php">Tables 2</a></li>
