361abf2beb68de69ddddf090e1d00361d8150d02
Jeremy Mathieu <g3raphacs@gmail.com>
2020-05-29 13:39:41 +0200

    structure navbar

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiply</title>
+    <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@400;700&family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div id="navbar">
+            <nav class="container flexspaceb m0child">
+                <div id="logo" class="flex">
+                        <div><img src="/media/multiply-logo.png" alt="" width="100" height="100"></div>
+                        <p>Multiply</p>
+                </div>
+                <menu class="flexspaceb">
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="tables.php">Tables</a></li>
+                    <li><a href="tables2.php">Tables 2</a></li>
+                    <li><a href="revision.php">Révision</a></li>
+                    <li><a href="superrevision.php">Super Révision</a></li>
+                </menu>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
