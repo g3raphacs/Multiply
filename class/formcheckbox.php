35c141bc345668679b0afe3f696f413fb798c204
Jeremy Mathieu <g3raphacs@gmail.com>
2020-05-31 17:56:14 +0200

    styling checkboxes

--- class/formcheckbox.php
+++ class/formcheckbox.php
@@ -1,10 +1,10 @@
         <!-- formulaire -->
         <div class="container txt-center">
-            <form class="flexcenter" action="" method="POST">
+            <form class="flexcenter wrap" action="" method="POST">
                 <?php
                     for($i=1 ; $i<11 ; $i++){
-                        echo '<input type="checkbox" name="tables[]" id="check'.$i.'" value="'.$i.'">';
-                        echo '<label for="check'.$i.'">'.$i.'</label>';
+                        echo '<input class="checkbox" type="checkbox" name="tables[]" id="check'.$i.'" value="'.$i.'">';
+                        echo '<label class="btn-table f-bold txt-table'.$i.'" for="check'.$i.'">'.$i.'</label>';
                     }
                 ?>
                 <button class="txt-white btn-submit" type="submit"><img src="/media/validate.svg" alt="" width="40" height="40"></button>
