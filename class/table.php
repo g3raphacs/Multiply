422fe0180ffb0acb095da379f0e42b0e5dea7331
Jeremy Mathieu <g3raphacs@gmail.com>
2020-05-31 23:36:43 +0200

    thiner tables design

--- class/table.php
+++ class/table.php
@@ -2,13 +2,13 @@
 class Table
 {
     public function __construct(int $n){
-        echo '<section id="table'.$n.'" class="tableSec txt-black">
-        <header class="bck-main-light">
-        <h2 class="txt-center flex f-18"><span class="txt-white">La table du </span><span class="numA bck-white txt-table'.$n.'">'.$n.'</span></h2>
+        echo '<section id="table'.$n.'" class="tableSec txt-main-black">
+        <header class="bck-main">
+        <h2 class="txt-center f-18 flex"><span class="txt-white">La table du </span><span class="numA bck-white txt-table'.$n.'">'.$n.'</span></h2>
         </header>
         <div class="table bck-white">';
             for($i=1 ; $i<11 ; $i++){
-                echo '<p class="flexspaceev"><span class="txt-table'.$n.'">'.$n.'</span> x <span>'.$i.'</span> = <span class="txt-white bck-table'.$n.'">'.($n*$i).'</span></p>';
+                echo '<p class="flexspaceev f-15"><span class="txt-table'.$n.'">'.$n.'</span> x <span>'.$i.'</span> = <span class="txt-white bck-table'.$n.'">'.($n*$i).'</span></p>';
             }
         echo '</div></section>';
     }
