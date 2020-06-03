1d640ecad849ccb019c6970ea8bf2d4c380ac8c5
Jeremy Mathieu <g3raphacs@gmail.com>
2020-06-03 10:48:40 +0200

    update questions

--- class/superquestion.php
+++ class/superquestion.php
@@ -12,7 +12,7 @@ class Question
                 <h2 class="txt-center f-18 flex"><span class="txt-white quest">Question '.$id.' :</span></h2>
             </header>
             <div class="table bck-white">
-                    <p class="flexspaceev f-15"><span class="txt-table'.$n.'">'.$n.'</span> x <span>'.$randnum.'</span> = <span class="txt-white bck-table'.$n. '"><input class="rep f-bold f-16 txt-white txt-center bck-table'.$n. '" type="number" placeholder="?" min="0" name="reponse[]" /></span></p>
+                    <p class="flexspaceev f-15"><span class="txt-table'.$n.'">'.$n.'</span> x <span>'.$randnum.'</span> = <span class="txt-white bck-table'.$n. '"><input class="rep f-bold f-16 txt-white txt-center bck-table'.$n. '" type="number" placeholder="?" min="0" name="reponse[]" required /></span></p>
                     <input type="hidden" name="result[]" value="'.($randnum*$n).'" />
                     <input type="hidden" name="val1[]" value="'.$n.'" />
                     <input type="hidden" name="val2[]" value="'.$randnum.'" />
--- class/superreponse.php
+++ class/superreponse.php
@@ -4,19 +4,21 @@ class Reponse
 {
     public $randnum = 0;
     public function __construct(int $val1 , int $val2 , int $reponse ){
-        $coul;
-        if($reponse==($val1*$val2)){
-            $coul=4;
-            global $score;
-            $score=$score+1;
-        }
-        else{$coul=8;}
+
 
         echo
-        '<section id="table'.$val1.'" class="tableSec txt-main-black">
-            <header class="bck-table'.$coul.'">
-                <h2 class="txt-center f-18 flex"><span class="txt-white quest">Réponse : '.$reponse.'</span></h2>
-            </header>
+        '<section id="table'.$val1.'" class="tableSec txt-main-black">';
+
+            if($reponse==($val1*$val2)){
+                echo'<header class="bck-table4"><h2 class="txt-center f-18 flex"><span class="txt-white quest">Réponse : '.$reponse.'</span></h2><p class="txt-center"><img src="media/happy.svg" alt="" width="45"></p>';
+                global $score;
+                $score=$score+1;
+            }
+            else{
+                echo'<header class="bck-table8"><h2 class="txt-center f-18 flex"><span class="txt-white quest">Réponse : '.$reponse.'</span></h2><p class="txt-center"><img src="media/sad.svg" alt="" width="45"></p>';
+            }
+
+        echo'</header>
             <div class="table bck-white">
                     <p class="flexspaceev f-15"><span class="txt-table'.$val1.'">'.$val1.'</span> x <span>'.$val2.'</span> = <span class="txt-white bck-table'.$val1.'">'.($val1*$val2).'</span></p>
             </div>
